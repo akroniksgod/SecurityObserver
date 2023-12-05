1cb08149907e3f698a3c7cda8cdf7f519f237279
VadimKarelov <vadimkarelov02@gmail.com>
2023-12-05 14:51:20 +0500

    Добавил шаблон общего проекта

---
+++ SecurirtyObserver.Common/SecurityObserver.Common/Requests/RequestStorage.cs
@@ -0,0 +1,16 @@
+﻿namespace SecurityObserver.Common.Requests;
+
+/// <summary>
+/// Хранит пути запросов. Создан, чтобы они были унифицированы для всех частей проекта.
+/// </summary>
+public class RequestStorage
+{
+    #region Запросы для приложения RaspberryPI
+
+    /// <summary>
+    /// Получает QR code в виде строки, должен возвращать логическое значение true, если проход раазрешен.
+    /// </summary>
+    public string CheckQrCode => "/api/CheckQrCode/code={code}";
+
+    #endregion
+}
