5de947fefadc88c797a1f374a01f96047895d45b
TimCheers <artem1978354@gmail.com>
2023-12-16 17:44:19 +0500

    Изменил названия переменных и функций согласно требованиям Александра Тедеева

--- SecurityObserverPy/Models/EntranceCode.py
+++ SecurityObserverPy/Models/EntranceCode.py
@@ -8,8 +8,8 @@ Base = declarative_base()
 class EntranceCode(Base):
     __tablename__ = 'entrance_code'
     id = Column(Integer, primary_key=True)
-    employee_id = Column(Integer, ForeignKey('employee.id'))
-    creation_date = Column(DateTime(), default=datetime.now)
+    employeeId = Column(Integer, ForeignKey('employee.id'))
+    creationDate = Column(DateTime(), default=datetime.now)
 
     __table_args__ = (
         PrimaryKeyConstraint('id', name='entrance_code_pk'),
